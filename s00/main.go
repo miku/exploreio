15b38974a94038d442a9a9964ac912eb5510ca2c
Martin Czygan <martin.czygan@gmail.com>
2017-02-25 16:44:32 +0100

    update example

--- s00/main.go
+++ s00/main.go
@@ -2,10 +2,15 @@
 //
 // It has been asked, how to get an reader from a file:
 //
-// "golang: Create a io.Reader from a local file"
-// http://stackoverflow.com/questions/25677235/golang-create-a-io-reader-from-a-local-file
+//     "golang: Create a io.Reader from a local file"
+//         http://stackoverflow.com/q/25677235/89391
 //
-// Indeed, *os.File implements a Read methods with the approriate signature, so *os.File is an io.Reader.
+//     > asked 2014-09-05, 01:05:30Z, viewed 16338 times
+//
+// Indeed, *os.File implements a `Read` method with the approriate signature,
+// so *os.Fileis an io.Reader.
+//
+// OUTPUT:
 //
 //     $ go run main.go
 //     31 bytes read: Concurrency is not parallelism.
@@ -18,20 +23,20 @@ import (
 	"os"
 )
 
-// TODO: Change a single character so the complete file is read and printed.
+// TODO: Change a single character in this program so the complete file is read and printed.
 func main() {
 	file, err := os.Open("hello.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// A file is an io.Reader.
 	var r io.Reader = file
 
-	b := make([]byte, 1024)
+	b := make([]byte, 11)
 	n, err := r.Read(b)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d bytes read: %s\n", n, string(b))
-	// Output: 11 bytes read: Concurrency
 }
--- s06/main.go
+++ s06/main.go
@@ -1,5 +1,7 @@
 // S06: Besides Marshaling, JSON (and XML) can also be decoded.
 //
+// OUTPUT:
+//
 //     $ cat hello.json | go run main.go
 //     It's around 2017-01-20 17:15:54.603712222 +0100 CET now and ...
 //     we are at wonderful Golab 2017 in Firenze! @golab_conf
@@ -20,11 +22,11 @@ type record struct {
 }
 
 func main() {
-	// TODO: Unmarshal from Stdin into a record struct. 4 lines with error handling.
-	var rec record
-	if err := json.NewDecoder(os.Stdin).Decode(&rec); err != nil {
-		log.Fatal(err)
-	}
+	// TODO: Unmarshal from Stdin into a record struct (4 lines).
+	// ...
+	// ...
+	// ...
+	// ...
 	fmt.Printf("It's around %s now and we are at wonderful %s in %s! @golab_conf\n",
 		rec.Date, rec.Name, rec.Location)
 }
