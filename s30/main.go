81f09919fe1483c7fe3193649b5238af075f4488
Martin Czygan <martin.czygan@gmail.com>
2017-03-02 02:22:55 +0100

    buffer example

--- s30/main.go
+++ s30/main.go
@@ -7,33 +7,38 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
-// Buffer minimal.
+// Buffer is a minimal implementation of a variable size byte slice one can read
+// from and write into.
 type Buffer struct {
 	b    []byte
 	i, j int
 }
 
-// Len TODO(miku): Check.
+// Len returns the current length of this buffer.
 func (b *Buffer) Len() int {
 	return b.j - b.i
 }
 
-// Cap TODO(miku): Check.
+// Cap returns the current capacity of this buffer.
 func (b *Buffer) Cap() int {
 	return len(b.b)
 }
 
+// Write writes given byte slice to the buffer.
 func (b *Buffer) Write(p []byte) (n int, err error) {
 	b.b = append(b.b, p...)
 	b.j += len(p)
 	return len(p), nil
 }
 
+// Read reads the current buffer.
 func (b *Buffer) Read(p []byte) (n int, err error) {
 	sz := len(b.b) - b.i
 	if sz > len(p) {
@@ -41,21 +46,20 @@ func (b *Buffer) Read(p []byte) (n int, err error) {
 	} else {
 		err = io.EOF
 	}
-	copy(p, b.b[b.i:])
+	copy(p, b.b[b.i:b.i+sz])
 	b.i += sz
 	return sz, err
 }
 
 func main() {
 	var buf Buffer
-	if _, err := io.WriteString(&buf, "\n░♡░┳┣I░A░Ⓝ░К▒❀▒УOЦ░♡░ for your attention!\n\n"); err != nil {
+	if _, err := io.Copy(&buf, os.Stdin); err != nil {
 		log.Fatal(err)
 	}
-	r := io.LimitReader(&buf, 6)
-	if _, err := io.Copy(os.Stdout, r); err != nil {
+	log.Printf("%d bytes read", buf.Len())
+	b, err := ioutil.ReadAll(&buf)
+	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := io.Copy(os.Stdout, &buf); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(string(b))
 }
